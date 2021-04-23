75ebf0731d092d047223f244a6ce8a0fec25763e
Kiddy204 <ilias96.lahrach@gmail.com>
2021-04-23 12:54:03 +0000

    sh256 betty-style

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <openssl/sha.h>
-uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]);
-
-#endif // HBLK_CRYPTO_H_INCLUDED
+uint8_t *sha256(int8_t const *s, size_t len,
+	uint8_t digest[SHA256_DIGEST_LENGTH]);
+#endif /* HBLK_CRYPTO_H_INCLUDED*/
 
