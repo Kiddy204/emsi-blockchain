70b894bd8484332042ae2ae1d3b10a7ce46030c6
Kiddy204 <ilias96.lahrach@gmail.com>
2021-04-23 12:49:51 +0000

    sh256 betty-style

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -1,5 +1,3 @@
-
-
 #include "hblk_crypto.h"
 
 /**
@@ -10,10 +8,11 @@
  *
  * Return: pointer to digest, NULL on error
  */
-uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]){
-	if(digest == NULL ){
-		return NULL;
-	}
-	return SHA256((const unsigned char *)s,len,digest);
+uint8_t *sha256(int8_t const *s, size_t len,
+		uint8_t digest[SHA256_DIGEST_LENGTH])
+{
+	if (!s || !digest)
+		return (NULL);
+	return (SHA256((const unsigned char *)s, len, digest));
 }
 
