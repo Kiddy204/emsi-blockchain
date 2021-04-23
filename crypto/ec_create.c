3a943098f1a74c5cead7e9ab53921d9003cc5673
Kiddy204 <ilias96.lahrach@gmail.com>
2021-04-23 14:41:16 +0000

    Ec_CREATE

--- crypto/ec_create.c
+++ crypto/ec_create.c
@@ -1,21 +1,28 @@
-/** EC_create - creates returns a public private EC key
- *@Return a pointer to EC_KEY structure
-*/
+
 #include "hblk_crypto.h"
-EC_KEY *ec_create() {
+/**
+ * EC_create - creates returns a public private EC key
+ *
+ *  Return a pointer to EC_KEY structure
+ */
+
+EC_KEY *ec_create()
+{
 	EC_KEY *key;
 	/*Create a new EC SECP256 Structure */
 	key = EC_KEY_new_by_curve_name(EC_CURVE);
 	if (!key)
 	{
-		fprintf(stderr,"EC Curve was not² successfully generated");
-		return NULL;
+		fprintf(stderr, "EC Curve was not² successfully generated");
+		return (NULL);
 	}
 	/*Generate Public and private Keys*/
 	EC_KEY_generate_key(key);
-	if(!key){
-		fprintf(stderr," Key's were not successfully generate");
+	if (!key)
+	{
+		fprintf(stderr, " Key's were not successfully generate");
+		return (NULL);
 	}
-	return key;
+	return (key);
 }
 
