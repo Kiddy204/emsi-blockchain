381b254fecd4cbb9804bcdc0841d03654fe9b4ae
Kiddy204 <ilias96.lahrach@gmail.com>
2021-04-24 14:41:30 +0000

    add ec_load

---
+++ crypto/ec_load.c
@@ -0,0 +1,54 @@
+#ifndef EC_LOAD_C_INCLUDED
+#define EC_LOAD_C_INCLUDED
+
+
+#include "hblk_crypto.h"
+
+/**
+ * ec_load - load an EC key pair from the disk
+ * @folder: path to pub and pvt keys
+ * Return: pointer to created EC key pair, NULL on error
+ */
+EC_KEY *ec_load(char const *folder)
+{
+	EC_KEY *key = NULL;
+	char buf[BUFSIZ];
+	FILE *fp;
+
+	if (!folder)
+    {
+        fprintf(stderr, "folder param not found");
+        return (NULL);
+	}
+
+	sprintf(buf, "%s/%s", folder, PUB_FILENAME);
+	fp = fopen(buf, "r");
+	if (!fp)
+        {
+        fprintf(stderr, "no File File %s ",buf);
+		return (NULL);
+        }
+	if (!PEM_read_EC_PUBKEY(fp, &key, NULL, NULL))
+        {
+        fprintf(stderr, "FAIL to load PubKey ");
+		goto out;
+        }
+	sprintf(buf, "%s/%s", folder, PRI_FILENAME);
+	fp = fopen(buf, "r");
+	if (!fp)
+        {
+        fprintf(stderr, "no File File %s ",buf);
+		return (NULL);
+        }
+	if (!PEM_read_ECPrivateKey(fp, &key, NULL, NULL))
+        {
+        fprintf(stderr, "FAIL to load PvtKey ");
+		goto out;
+        }
+out:
+    fclose(fp);
+	return (key);
+}
+
+#endif // EC_LOAD_C_INCLUDED
+
