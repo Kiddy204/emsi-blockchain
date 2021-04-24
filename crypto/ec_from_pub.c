3e7072432e03db8aee4e29d8446b8a5baec8e816
Kiddy204 <ilias96.lahrach@gmail.com>
2021-04-24 10:28:37 +0000

    add ec_from_pub

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -3,7 +3,7 @@
  *ec_from_pub - creates an EC_KEY structure given a public key
  *@pub  : public key to convert
  *
- *Return : pointer to an EC_KEY
+ *Return: pointer to an EC_KEY
  */
 
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
