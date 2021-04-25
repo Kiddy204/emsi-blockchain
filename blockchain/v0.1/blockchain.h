ece963dce8722559e39036530d989d2d59912886
Kiddy204 <ilias96.lahrach@gmail.com>
2021-04-25 22:30:18 +0000

    add block_is_valid.c

--- blockchain/v0.1/blockchain.h
+++ blockchain/v0.1/blockchain.h
@@ -122,4 +122,5 @@ uint8_t *block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH]
 int blockchain_serialize(blockchain_t const *blockchain, char const *path);
 blockchain_t *blockchain_deserialize(char const *path);
 llist_t *deserialize_blocks(int fd, uint32_t size, uint8_t endianness);
+int block_is_valid(block_t const *block, block_t const *prev_block);
 #endif
